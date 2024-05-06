b27fa49c2b55165b5f79070974f788822bfe02a9
ryanmutula <ryan.mutula@student.moringaschool.com>
2024-05-06 16:44:45 +0300

    jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>
+Meet Ryan, the software sorcerer with a knack for 
+turning bugs into features and coffee into code. 
+With a mind as agile as his fingers on the keyboard, 
+he's the wizard of web development, conjuring digital 
+wonders with a flick of his mouse. 
+Ryan: making software sorcery look easy!</p>
+   <img src={image} alt="I made this."/>
+  </div>;
 }
 
 export default About;
